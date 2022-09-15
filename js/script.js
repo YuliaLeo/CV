91b2eeb494a5f0a15e3a8237e2d3e19049f5f18b
YuliaLeo <89121084+YuliaLeo@users.noreply.github.com>
2022-09-15 07:28:07 +0700

    change_15_09

--- js/script.js
+++ js/script.js
@@ -1,63 +1,66 @@
 // это всё лишнее, потому как https://caniuse.com/webp 97%, причем я бы оформил это всё как полифилл
+//*Поняла, не смотрела, что поддержка уже почти 100%
+//*На счет полифилла, еще их сама не писала, посморю, как это делается
+
+const bodyEl = document.querySelector('body');
 
 function testWebP(callback) {
+	let webP = new Image();
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	const webpHeight = webP.height;
 
-	var webP = new Image();
 	webP.onload = webP.onerror = function () {
 		// что есть 2 здесь? магические цифры не приняты в коде
 		// а если вся эта функция откуда-то со stackoverflow то нужен и коммент со ссылкой на первоисточник
-		callback(webP.height == 2);
+		//* Она вот отсюда https://pastebin.com/CNgBiPz3, нашла достаточно давно, просто привыкла ее использовать
+		//* Цифра 2, потому что изображение 2x2, но сейчас понимаю, что она тут вообще не к чему
+		callback(webpHeight);
 	};
-	// Обычно выносится в константу, что это 1х1 пиксель
-	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+
+	return webpHeight;
 }
-
-testWebP(function (support) {
-	// if (support)
-	if (support == true) {
-		document.querySelector('body').classList.add('webp');
-	} else {
-		document.querySelector('body').classList.add('no-webp');
-	}
-
+let webpSupport = testWebP(function (support) {
+	const classes = support ? 'webp' : 'no-webp';
+	bodyEl.classList.add(classes);
 	// можно лучше этот же код сделать
 	// const bodyEl = document.querySelector('body'); // и этот bodyEl куда-нибудь наверх, вижу что ты его ещё ниже по коду используешь
 	// const classes = support ? 'webp' : 'no-webp';
 	// bodyEl.classList.add(classes);
+	// *Да, так намного лучше, хотя я немного изменила код и теперь этот блок уже в принципе не нужен
 });
 
 // всё ещё не знаю что такое ibg
+// *В html написала
 function ibg() {
 	// так, а здесь уже let, но выше видел var, определитесь
+	// *Та просто старая функция и я ее совсем не меняла, взяла, как она была, исправила
 	let ibgs = document.querySelectorAll('.ibg');
 
-	// лишняя проверка, цикл ни разу не выполнится на пустом ibgs
-	if (ibgs.length > 0) {
-		// есть перебор по querySelectorAll удобнее
-		// https://css-tricks.com/snippets/javascript/loop-queryselectorall-matches/
-		for (let index = 0; index < ibgs.length; index++) {
-			const ibg = ibgs[index];
+	// есть перебор по querySelectorAll удобнее
+	// https://css-tricks.com/snippets/javascript/loop-queryselectorall-matches/
 
-			if (ibg.querySelector("img")) {
-				ibg.style.backgroundImage = 'url("' + ibg.querySelector('img').src + '")';
-			}
+	ibgs.forEach(ibg => {
+		const webpSource = ibg.querySelector("source[type='image/webp']");
+		const imgSource = ibg.querySelector('img');
 
-			// мне не нравится этот участок кода, он перегружен проверками, каждую из которых стоит
-			// вынести в отдельную переменную, а то это сложно к чтению, такой код не должен будет пройти мимо твоего лида в будущем
-
-			// более того проверку
-			// document.querySelector("body").classList.contains("webp")
-			// можно сделать однажды ещё до запуска этой функции ibg
-
-			// ibg.querySelectorAll("source")[0]
-			// дублируешь трижды, не надо так
-			if (ibg.querySelectorAll("source")[0] && document.querySelector("body").classList.contains("webp")) {
-				if (ibg.querySelectorAll("source")[0].type === "image/webp") {
-					ibg.style.backgroundImage = 'url("' + ibg.querySelectorAll("source")[0].srcset + '")';
-				}
-			}
+		if (webpSupport && webpSource) {
+			ibg.style.backgroundImage = 'url("' + webpSource.srcset + '")';
 		}
-	}
+		else if (imgSource) {
+			ibg.style.backgroundImage = 'url("' + imgSource.src + '")';
+		}
+	});
+	// мне не нравится этот участок кода, он перегружен проверками, каждую из которых стоит
+	// вынести в отдельную переменную, а то это сложно к чтению, такой код не должен будет пройти мимо твоего лида в будущем
+
+	// более того проверку
+	// document.querySelector("body").classList.contains("webp")
+	// можно сделать однажды ещё до запуска этой функции ibg
+
+	// ibg.querySelectorAll("source")[0]
+	// дублируешь трижды, не надо так
+	//*Да, согласна, я тут намудрила очень много не нужного (вообще функцию ibg я брала из туториала на Ютуб около года назад 
+	// и модифицировала её под себя)
 }
 
 // не, это плохо по многим причинам.
@@ -66,4 +69,4 @@ function ibg() {
 // https://learn.javascript.ru/event-loop
 // 2) Зачем так изощряться, когда у тебя в js уже известен результат из функции testWebP (просто сделай return этого значения),
 // т.е. известно есть ли поддержка webp или нет
-setTimeout(ibg, 10); // чтобы класс .webp успел примениться
+ibg();
